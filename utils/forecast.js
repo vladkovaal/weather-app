67b4bfb4401fd6bbc6f04bcc2cd5a81a55deb65c
Vladyslav <kovalchuk.vlad01@gmail.com>
2022-07-28 07:40:24 +0300

    Refactored forecast.js
    Changed string assembling for url

--- utils/forecast.js
+++ utils/forecast.js
@@ -3,19 +3,20 @@ import request from 'request';
 const WEATHER_URL = 'http://api.weatherstack.com/current';
 const WEATHER_ACCESS = '93751fe074068be9c2bc742f70adb5a9';
 
-const forecast = (lat, lon, cb) => {
-	const url =
-		WEATHER_URL + '?access_key=' + WEATHER_ACCESS + '&query=' + lat + ',' + lon;
+const forecast = (latitude, longitude, cb) => {
+	const query = '?' + 'access_key=' + WEATHER_ACCESS + '&' + 'query=' + latitude + ',' + longitude;
+	const url = WEATHER_URL + query;
 
 	request({ url, json: true }, (error, { body }) => {
 		if (error) {
-			cb('Unable to connect to weatherstack!', undefined);
+			cb('Error occured while connecting to weather service. Please, try again later.', undefined);
 		} else if (body.error) {
 			cb(body.error.info, undefined);
 		} else {
 			cb(undefined, {
-				temperature: body.current.temperature,
-				feelslike: body.current.feelslike,
+				desc: body.current.weather_descriptions.join('. ') + '.',
+				temp: body.current.temperature,
+				precip: body.current.precip,
 			});
 		}
 	});
